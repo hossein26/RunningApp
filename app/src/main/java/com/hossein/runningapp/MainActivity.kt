c465244ebf252f2f34a9e12ac79aa153ceb4f16f
Hossein <hosseinbeigi26@gmail.com>
2021-09-29 20:12:50 +0330

    add Layouts

--- app/src/main/java/com/hossein/runningapp/MainActivity.kt
+++ app/src/main/java/com/hossein/runningapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hossein.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hossein.runningapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
