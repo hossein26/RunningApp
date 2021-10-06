d80f903d2aba53b5db23c617a6309fffc56dfba8
Hossein <hosseinbeigi26@gmail.com>
2021-10-06 18:02:56 +0330

    Making Foreground Service

--- app/src/main/java/com/hossein/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/hossein/runningapp/ui/MainActivity.kt
@@ -1,27 +1,35 @@
 package com.hossein.runningapp.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.hossein.runningapp.R
 import com.hossein.runningapp.databinding.ActivityMainBinding
 import androidx.navigation.fragment.findNavController
+import com.hossein.runningapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var navHostFragment:NavHostFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
 
         setSupportActionBar(binding.toolbar)
         val navController = findNavController(R.id.navHostFragment)
@@ -36,5 +44,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        navigateTrackingFragmentIfNeeded(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateTrackingFragmentIfNeeded(intent: Intent?){
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment?.findNavController()?.navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
