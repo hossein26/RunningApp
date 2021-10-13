8bea21b3bdac015c8deeaa97ae367d8d5baa0e05
Hossein <hosseinbeigi26@gmail.com>
2021-10-13 12:38:28 +0330

    Sorting Runs

--- app/src/main/java/com/hossein/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/hossein/runningapp/ui/fragments/RunFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.hossein.runningapp.R
 import com.hossein.runningapp.adapters.RunAdapter
 import com.hossein.runningapp.databinding.FragmentRunBinding
+import com.hossein.runningapp.other.SortType
 import com.hossein.runningapp.other.TrackingUtility.hasPermission
 import com.hossein.runningapp.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,6 +29,7 @@ class RunFragment : Fragment() {
     private var _binding: FragmentRunBinding? = null
     private val binding get() = _binding!!
     private lateinit var runAdapter: RunAdapter
+    private lateinit var spFilter: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +37,7 @@ class RunFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRunBinding.inflate(inflater, container, false)
+        spFilter = binding.spFilter
         return binding.root
     }
 
@@ -48,9 +53,31 @@ class RunFragment : Fragment() {
 
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                when(pos){
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    3 -> viewModel.sortRuns(SortType.DISTANCE)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
+
+        when(viewModel.sortType){
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.AVG_SPEED -> spFilter.setSelection(2)
+            SortType.DISTANCE -> spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spFilter.setSelection(4)
+        }
     }
 
     private fun setupRecyclerView() = binding.rvRuns.apply {
