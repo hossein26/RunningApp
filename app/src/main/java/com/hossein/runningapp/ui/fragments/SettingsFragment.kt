17749701689e9352112da14c4e75a6fd7cabc643
Hossein <hosseinbeigi26@gmail.com>
2021-10-14 11:39:15 +0330

    Settings Fragment

--- app/src/main/java/com/hossein/runningapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/hossein/runningapp/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.hossein.runningapp.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,15 +10,25 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.hossein.runningapp.R
 import com.hossein.runningapp.databinding.FragmentRunBinding
 import com.hossein.runningapp.databinding.FragmentSettingsBinding
+import com.hossein.runningapp.other.Constants.KEY_NAME
+import com.hossein.runningapp.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment: Fragment() {
 
     private val viewModel: ViewModel by viewModels()
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +37,49 @@ class SettingsFragment: Fragment() {
     ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadFieldsFromSharedPref()
+
+        binding.btnApplyChanges.setOnClickListener {
+            val success = applyChangeToSharedPref()
+            if (success){
+                Snackbar.make(view, "saved changed!", Snackbar.LENGTH_SHORT).show()
+            }else{
+                Snackbar.make(view, "please fill out all the fields", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadFieldsFromSharedPref(){
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+
+        binding.etName.setText(name)
+        binding.etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangeToSharedPref(): Boolean{
+
+        val name = binding.etName.text.toString()
+        val weight = binding.etWeight.text.toString()
+
+        if (name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+
+        sharedPreferences.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .apply()
+
+        val toolbarText = "Let's go $name"
+        requireActivity().tvToolbarTitle.text = toolbarText
+
+        return true
     }
 
     override fun onDestroyView() {
